22bffc176387ebdb2a9d2612c3064e9fc1417fb2
Arbe <bektemir1102@gmail.com>
2026-05-26 21:34:42 +0600

    Changes to code lines for async.

--- app/user/models.py
+++ app/user/models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, String, Integer, Boolean, DateTime, func, Numeric, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 from app.database import Base
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
@@ -18,16 +19,41 @@ class User(Base):
     date_joined = Column(DateTime, default=func.now())
     last_login = Column(DateTime, nullable=True)
 
-    posts = relationship('Post', back_populates='user')
+    posts: Mapped[list['Post']] = relationship(
+        'Post',
+        back_populates='user',
+        lazy='selectin'
+    )
 
-    followers_rel = relationship('Follower', foreign_keys='Follower.following_id', back_populates='following')
-    following_rel = relationship('Follower', foreign_keys='Follower.follower_id', back_populates='follower')
+    followers_rel: Mapped[list['Follower']] = relationship(
+        'Follower',
+        foreign_keys='Follower.following_id',
+        back_populates='following',
+        lazy='selectin'
+    )
+    following_rel: Mapped[list['Follower']] = relationship(
+        'Follower',
+        foreign_keys='Follower.follower_id',
+        back_populates='follower',
+        lazy='selectin'
+    )
 
 
 class Follower(Base):
     __tablename__ = 'followers'
+
     follower_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     following_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
 
-    follower = relationship('User', foreign_keys=[follower_id], back_populates='following_rel')
-    following = relationship('User', foreign_keys=[following_id], back_populates='followers_rel')
+    follower: Mapped['User'] = relationship(
+        'User',
+        foreign_keys=[follower_id],
+        back_populates='following_rel',
+        lazy='selectin'
+    )
+    following: Mapped['User'] = relationship(
+        'User',
+        foreign_keys=[following_id],
+        back_populates='followers_rel',
+        lazy='selectin'
+    )
