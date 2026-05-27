6e2d688a5b0b193c0d9098bd5945459a4711935d
Arbe <bektemir1102@gmail.com>
2026-05-27 15:45:27 +0600

    to make the del query work well

--- app/posts/models.py
+++ app/posts/models.py
@@ -13,9 +13,11 @@ class Post(Base):
     views = Column(Integer, default=0)
 
     user = relationship('User', back_populates='posts')
+    likes = relationship('Like', back_populates='post', cascade='all, delete-orphan')
 
 class Like(Base):
     __tablename__ = 'likes'
 
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
+    post = relationship('Post', back_populates='likes')
