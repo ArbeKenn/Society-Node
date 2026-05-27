f8bbc71c3adf4f42b623f764780540a58dde8c43
Arbe <bektemir1102@gmail.com>
2026-05-27 15:42:16 +0600

    adjusted for async.

--- app/posts/router.py
+++ app/posts/router.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+
 from app.database import get_db
-from app.posts.models import Post as PostModel, Like as LikeModel
+from app.posts.models import (
+    Post as PostModel,
+    Like as LikeModel
+)
 from app.posts.schemas import PostCreateUpdateSchema
 from app.user.jwt import get_current_user
 
@@ -16,103 +21,142 @@ limiter = Limiter(key_func=get_remote_address)
 
 @router.get('/posts')
 @limiter.limit('1/sec')
-def all_posts(request: Request, db: Session = Depends(get_db)):
-    posts = db.query(PostModel).limit(10)
+async def all_posts(
+        request: Request,
+        db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(PostModel))
+    posts = result.scalars().all()
+
     for post in posts:
         post.views +=1
-        db.commit()
-        db.refresh(post)
+    await db.commit()
     return posts
 
-@router.get('posts/{post_id}')
+@router.get('/posts/{post_id}')
 @limiter.limit('1/sec')
-def detail_post(request: Request, post_id: int, db: Session = Depends(get_db)):
-    post = db.query(PostModel).filter(PostModel.id == post_id).first()
+async def detail_post(
+        request: Request,
+        post_id: int, db:
+        AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(PostModel)
+        .where(PostModel.id == post_id)
+    )
+    post = result.scalar_one_or_none()
+    if not post:
+        raise HTTPException(
+            status_code=404,
+            detail='Post not found'
+        )
+
     post.views += 1
-    db.commit()
-    db.refresh(post)
+    await db.commit()
+    await db.refresh(post)
     return post
 
-@router.post('posts/{post_id}/like')
-def like(
+@router.post('/posts/{post_id}/like')
+@limiter.limit('1/sec')
+async def like(
+        request: Request,
         post_id: int,
-        db: Session = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         user_id: int =Depends(get_current_user)
 ):
-    post = db.query(PostModel).filter(PostModel.id == post_id).first()
+    result = await db.execute(
+        select(PostModel)
+        .where(PostModel.id == post_id)
+    )
+    post = result.scalar_one_or_none()
     if not post:
         raise HTTPException(
             status_code=404,
             detail='Post Not Found'
         )
-    existing = db.query(LikeModel).filter_by(
-        user_id=user_id,
-        post_id=post_id
-    ).first()
+    result = await db.execute(
+        select(LikeModel)
+        .where(LikeModel.user_id == user_id, LikeModel.post_id == post_id)
+    )
+
+    existing = result.scalar_one_or_none()
 
     if existing:
-        db.delete(existing)
+        await db.delete(existing)
         post.like -= 1
-        db.commit()
+        await db.commit()
         return {'status': 'unliked'}
 
     else:
         db.add(LikeModel(user_id=user_id, post_id=post_id))
         post.like += 1
-        db.commit()
+        await db.commit()
         return {'status': 'liked'}
 
 @router.post('/new_post')
-def create_post(
+@limiter.limit('1/sec')
+async def create_post(
+        request: Request,
         post: PostCreateUpdateSchema,
-        db: Session = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         user_id: int = Depends(get_current_user)
 ):
 
     new_post = PostModel(**post.model_dump())
     new_post.user_id = user_id
     db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
+    await db.commit()
+    await db.refresh(new_post)
 
     return new_post
 
 
 @router.put('/edit/{post_id}')
-def edit_post(
+@limiter.limit('1/sec')
+async def edit_post(
+        request: Request,
         post_id: int,
         post_schema: PostCreateUpdateSchema,
-        db: Session = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         user_id: int = Depends(get_current_user)
 ):
 
-    post = db.query(PostModel).filter(
-        PostModel.id == post_id
-    ).first()
+    result = await db.execute(
+        select(PostModel)
+        .where(PostModel.id == post_id)
+    )
+    post = result.scalar_one_or_none()
 
     if not post:
         raise HTTPException(
             status_code=404,
             detail='Post Not Found')
+    if post.user_id != user_id:
+        raise HTTPException(
+            status_code=403,
+            detail='Not your post')
 
     for key, value in post_schema.model_dump().items():
         setattr(post, key, value)
 
     post.user_id = user_id
-    db.commit()
-    db.refresh(post)
+    await db.commit()
+    await db.refresh(post)
     return post
 
 @router.delete('/del/{post_id}')
-def del_post(
+@limiter.limit('1/sec')
+async def del_post(
+        request: Request,
         post_id: int,
-        db: Session = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         user_id: int = Depends(get_current_user)
 ):
 
-    post = db.query(PostModel).filter(
-        PostModel.id == post_id
-    ).first()
+    result = await db.execute(select(PostModel).where(
+        PostModel.id == post_id)
+    )
+    post = result.scalar_one_or_none()
 
     if not post:
         raise HTTPException(
@@ -125,6 +169,6 @@ def del_post(
             detail='Not your post'
         )
 
-    db.delete(post)
-    db.commit()
+    await db.delete(post)
+    await db.commit()
     return {'message': 'Post deleted'}
