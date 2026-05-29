63ba201d7a22784854658450f362d17d0a9ab3f1
Arbe <bektemir1102@gmail.com>
2026-05-29 19:06:49 +0600

    news tables comment. many to many

--- app/posts/models.py
+++ app/posts/models.py
@@ -13,11 +13,35 @@ class Post(Base):
     views = Column(Integer, default=0)
 
     user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post', cascade='all, delete-orphan')
     likes = relationship('Like', back_populates='post', cascade='all, delete-orphan')
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'))
+    like = Column(Integer, default=0)
+
+    post = relationship('Post', back_populates='comments')
+    likes = relationship('CommentLike', back_populates='comment', cascade='all, delete-orphan')
+
 
 class Like(Base):
     __tablename__ = 'likes'
 
     user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
+
     post = relationship('Post', back_populates='likes')
+
+
+class CommentLike(Base):
+    __tablename__ = 'comment_likes'
+
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    comment_id =Column(Integer, ForeignKey('comments.id', ondelete='CASCADE'), primary_key=True)
+
+    comment = relationship('Comment', back_populates='likes')
