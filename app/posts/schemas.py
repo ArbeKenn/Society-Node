268f581b7c545511408096b63797ec48c48f534b
Arbe <bektemir1102@gmail.com>
2026-06-08 11:20:01 +0600

    added Filed for length

--- app/posts/schemas.py
+++ app/posts/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CommentCreateUpdateSchema(BaseModel):
     text: str
@@ -10,8 +10,8 @@ class CommentResponseSchema(BaseModel):
     like: int
 
 class PostCreateUpdateSchema(BaseModel):
-    title: str
-    description: str | None
+    title: str = Field(min_length=4, max_length=128)
+    description: str
 
 class PostResponseSchema(BaseModel):
     id: int
