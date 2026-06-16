86c0cb6e7757e349c3681820779ecb51eddbaa50
Arbe <bektemir1102@gmail.com>
2026-06-16 22:15:34 +0600

    new endpoint for search items in shop. limiter

--- app/search/router.py
+++ app/search/router.py
@@ -1,21 +1,30 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, or_
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from app.database import get_db
 from app.posts.models import Post as PostModel
 from app.user.models import User as UserModel
+from app.shop.models import ShopItem as ShopItemModel
+
 from app.user.schemas import UserResponseSchema
 from app.posts.schemas import PostResponseSchema
 from app.search.schemas import SearchSchema
+from app.shop.schemas import ShopItemSchema
 
 router = APIRouter(
     prefix='/search',
     tags=['Search']
 )
 
+limiter = Limiter(key_func=get_remote_address)
+
 @router.get('/post', response_model=list[PostResponseSchema])
+@limiter.limit('50/1seconds')
 async def search_post(
+        request: Request,
         schemas: SearchSchema = Depends(),
         db: AsyncSession = Depends(get_db)
 ):
@@ -29,7 +38,9 @@ async def search_post(
     return posts
 
 @router.get('/user', response_model=list[UserResponseSchema])
+@limiter.limit('50/1seconds')
 async def search_user(
+        request: Request,
         schemas: SearchSchema = Depends(),
         db: AsyncSession = Depends(get_db)
 ):
@@ -42,6 +53,16 @@ async def search_user(
     users = result.scalars().all()
     return users
 
-@router.get('/shop')
-async def search_in_shop():
-    pass
+@router.get('/shop', response_model=ShopItemSchema)
+@limiter.limit('50/1seconds')
+async def search_in_shop(
+        request: Request,
+        schemas: SearchSchema = Depends(),
+        db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(ShopItemModel)
+        .where(or_(ShopItemModel.title.ilike(f'%{schemas.q}%')))
+        .offset(schemas.offset)
+        .limit(schemas.limit)
+    )
